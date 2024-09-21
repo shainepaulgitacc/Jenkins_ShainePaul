69784977e043987d25cb13b8a197e5e7b5c00ff7
shainepaulgitacc <shainepaulproject@gmail.com>
2024-09-21 11:12:08 +0800

    Add Person.cs file and create Name property

---
+++ Jenkins_ShainePaul/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_ShainePaul
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+    }
+}
